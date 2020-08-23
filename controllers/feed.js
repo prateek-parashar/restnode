7e8b4552522af520f88c4bc2b3bda7276c440173
prateek-parashar <parashar4111@gmail.com>
2020-08-23 17:00:40 +0530

    Created the get and post methods to test the functionality of the Rest API service with Postman

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,15 @@
 exports.getPosts = (req, res, next) => {
-    res.send("Hello there");
+    res.status(200).json({
+        posts: [{ title: "First Post", content: "This is the first post" }],
+    });
+};
+
+exports.createPosts = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    //create a post in the database
+    res.status(201).json({
+        message: "Post created successfully",
+        post: { id: "helloThere", title: title, content: content },
+    });
 };
