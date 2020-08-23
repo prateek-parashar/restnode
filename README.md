587bfc8c155bd4e483c9d3432193b10e447c0d38
prateek-parashar <parashar4111@gmail.com>
2020-08-24 00:29:52 +0530

    Added comments and handled CORS errors

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # restnode
-Developing REST apis with node. 
+
+Developing REST apis with node.
+
+### Rest API
+
+-   First time I had a real explanation for the `CORS` problem.
--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
+// Settinng the headers in all the responses to allow CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+
+    next();
+});
+
 const feedRoutes = require("./routes/feed");
 
 app.use("/feed", feedRoutes);
