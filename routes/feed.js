ffb45f300e6e8f26473fc1dbb6de6045ce7327b9
prateek-parashar <parashar4111@gmail.com>
2020-09-01 00:35:25 +0530

    Imported code from Max

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,45 @@
-const express = require("express");
+const express = require('express');
+const { body } = require('express-validator/check');
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-const feedController = require("../controllers/feed");
+// GET /feed/posts
+router.get('/posts', isAuth, feedController.getPosts);
 
-router.get("/posts", feedController.getPosts);
+// POST /feed/post
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
 
-router.post("/posts", feedController.createPosts);
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
